e0c16b9512fe19579aa14e3c91b520fefab8f64d
Frank Hale <frankhale@gmail.com>
2024-11-25 21:30:53 -0500

    Add step progress bar; fix percent complete formatting

---
+++ src/app/step-progress/step-progress.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step-progress',
+  imports: [],
+  templateUrl: './step-progress.component.html',
+  styleUrl: './step-progress.component.scss'
+})
+export class StepProgressComponent {
+
+}
